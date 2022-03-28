b00c3fe363ec97779da86e5efad9a20b4ba9d47d
Jst77 <justinte7@gmail.com>
2022-03-28 14:38:15 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,62 @@
+function picture1() { 
+    window.open(href="yeezy1.html",'_blank');
+
+}
+
+function picture2() { 
+    window.open(href="yeezy2.html",'_blank');
+}
+
+function picture3() { 
+    window.open(href="yeezy3.html", '_blank')
+}
+
+function picture4() { 
+    window.open(href="yeezy4.html",'_blank');
+
+}
+
+function picture5() { 
+    window.open(href="yeezy5.html",'_blank');
+
+}
+
+function picture6() { 
+    window.open(href="yeezy6.html",'_blank');
+
+}
+
+function picture7() { 
+    window.open(href="nikeaf1.html",'_blank');
+
+}
+
+function picture8() { 
+    window.open(href="nikesbdunk.html",'_blank');
+
+}
+function picture9() { 
+    window.open(href="jordan5.html",'_blank');
+
+}
+function picture10() { 
+    window.open(href="jordan12.html",'_blank');
+
+}
+function picture11() { 
+    window.open(href="jordan4.html",'_blank');
+
+}
+function picture12() { 
+    window.open(href="jordan11.html",'_blank');
+
+}
+
+// add to cart 
+
+function addcart() { 
+    alert('Your cart has been updated')
+}
+
+
+
